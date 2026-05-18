b462f472c691cc67dd0298b3a7299e4b989eac37
dantesillo <dantecanales@liceovvh.cl>
2026-05-18 15:37:48 -0400

    ok

---
+++ STACK.JS/01_introduccion/static/script.js
@@ -0,0 +1,12 @@
+console.log("prueba de conexion con js")
+
+
+function cambiarTexto() {
+    let titulo ="hola, bienvenido"
+    if (document.getElementById("titulo").innerText == titulo);
+    document.getElementById("titulo").innerText = "has cambiado el texto con js";
+    document.getElementById("titulo").style.color="red";
+    
+    
+    
+}
