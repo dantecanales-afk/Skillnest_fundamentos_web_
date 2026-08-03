18bf99029f227309c4ebde2bfdadee24243de71c
dantesillo <dantecanales@liceovvh.cl>
2026-08-03 14:35:29 -0400

    ok

--- STACK.JS/11_funciones/static/js/script.js
+++ STACK.JS/11_funciones/static/js/script.js
@@ -3,26 +3,64 @@ console.log("conexion exitosa")
 // //ejemplo saludar sin parametros ()
 // function saludar() {
 //     alert("¡Hola, bienvenido!");
-    //funciones com parametros
+//funciones com parametros
 
-    function saludar(nombres) {
-        alert("Hola," + nombres + "!")
-    
+function saludar(nombres) {
+    alert("Hola," + nombres + "!")
+
     saludar("luis")
     saludar("ana")
 }
 
-function encontrarMaximo(a, b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;       
+function encontrarMayor() {
+    function encontrarMaximo(a, b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+        let numero1 = 10;
+        let numero2 = 7;
+        let maximo = encontrarMayor(numero1, numero2);
+        alert(`el numero mayor entre ${numero1} y ${numero2} es ${maximo}`);
     }
+
 }
-let numero1 = 10;
-let numero2 = 7;
-let maximo = encontrarMaximo(numero1, numero2);
-console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
+
+
+function suma(a, b, c,) {
+    return a + b - c
+} function mostrarResultado() {
+    let num1 = parseInt(prompt("ingrese valor"))
+    let num2 = parseInt(prompt("ingrese valor"))
+    let num3 = parseInt(prompt("ingrese valor"))
+    let resultado = suma(num1, num2, num3)
+    alert(`la ejecucion de ${num1} + ${num2} - ${num3} = ${resultado}`)
+}
 
 
 
+/*crea una funcion que reciba un parametro 
+y permita a traves de un bucle contar hasta este
+
+ej: se recibe el numero 5 y muestra : 1-2-3-4-5*/
+/*añadir una condicion que el  valor no sea mayor a 100*/ 
+
+function contarHasta(numero) {
+    let numeros = [];
+    for (let i = 1; i <= numero; i++) {
+        numeros.push(i);
+    }
+
+    return numeros.join("-");
+}
+function mostrarConteo(){
+    let num = parseInt(prompt("ingrese un numero"))
+    if(num <=100){
+    let resultado = contarHasta(num);
+    alert(`el conteo es ${resultado}`)
+}
+else{
+    alert("ingrese un valor menor o igual a 100 para que el vicente se calle")
+}
+}
---
+++ STACK.JS/12_click/static/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("colorButton").addEventListener("click", function () {
+    cambiarColor();
+});
+
+// Función que cambia el color de fondo al hacer clic
+function cambiarColor() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = colorAleatorio;
+}
